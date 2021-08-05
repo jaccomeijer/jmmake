619352e4f829cb916a0d03d29a7329c54ecb45a1
Jacco Meijer <jacco@jaccomeijer.nl>
2021-08-05 16:17:01 +0200

    chore: remove react refs

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['react-hooks', 'eslint-plugin-tsdoc'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 0,
@@ -27,16 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/camelcase': 0,
-    'react/react-in-jsx-scope': 0,
-    'react-hooks/rules-of-hooks': 2,
-    'react-hooks/exhaustive-deps': 1,
     'import/order': 2,
     'import/no-unresolved': 0,
-    'tsdoc/syntax': 'warn',
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
 }
