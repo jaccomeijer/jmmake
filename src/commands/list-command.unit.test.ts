a7447ddab18eba424bf73058ab4ecb8d9aaab3dc
Jacco Meijer <jacco@jaccomeijer.nl>
2021-09-26 14:15:05 +0200

    chore: add list comaand unit test

---
+++ src/commands/list-command.unit.test.ts
@@ -0,0 +1,24 @@
+import { listCommand } from './list-command'
+
+describe('List command should', () => {
+  const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
+  beforeEach(() => {
+    consoleSpy.mockReset()
+  })
+
+  test('list single repo packages', async () => {
+    await listCommand({
+      monoRepoPath: './src/commands/__fixtures__/list-command-single-repo',
+    })
+    expect(consoleSpy.mock.calls).toEqual([['list-command-single-repo']])
+  })
+
+  test('list mono repo packages', async () => {
+    await listCommand({
+      monoRepoPath: './src/commands/__fixtures__/list-command-mono-repo',
+    })
+    expect(consoleSpy.mock.calls[0][0]).toMatch(/package-[abc]/)
+    expect(consoleSpy.mock.calls[1][0]).toMatch(/package-[abc]/)
+    expect(consoleSpy.mock.calls[2][0]).toMatch(/package-[abc]/)
+  })
+})
